be8730e381781bed37306d1cb3a72437db82d5d2
Denis <SDN2705@ya.ru>
2024-02-19 01:02:35 +0500

    Overloading of __str__ method

--- objects_and_classes.py
+++ objects_and_classes.py
@@ -1,3 +1,4 @@
+
 class Student:
     def __init__(self, name, surname, gender):
         self.name = name
@@ -7,6 +8,15 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
         self.courses_attached = []
+
+
+    def average_grade(self):
+        average = sum(sum(grade_list) / len(grade_list) for grade_list in self.grades.values())
+        return average
+
+    def __str__(self):
+        return f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.average_grade():.2f}\nКурсы в процессе изучения: {self.courses_in_progress}\nЗавершенные курсы: {self.finished_courses}"
+
 
     def rate_lec(self, teacher, course, grade):
         if isinstance(teacher, Lecturer) and course in self.courses_attached and course in teacher.courses_in_progress:
@@ -28,6 +38,12 @@ class  Lecturer(Mentor):
         super().__init__(name, surname)
         self.grades = {}
         self.courses_in_progress = []
+    def average_grade(self):
+        for grade_list in self.grades.values():
+            average = sum(grade_list) / len(grade_list)
+        return average
+    def __str__(self):
+        return f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.average_grade():.2f}"
 
 class  Reviewer(Mentor):
 
@@ -39,10 +55,13 @@ class  Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
-
+    def __str__(self):
+        return f"Имя: {self.name}\nФамилия: {self.surname}"
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
+best_student.courses_in_progress += ['Git']
+best_student.finished_courses += ['Введение в программирование']
 
 best_teacher = Lecturer('Clark', 'Kent')
 best_teacher.courses_in_progress += ['Python']
@@ -55,12 +74,19 @@ cool_reviewer.courses_attached += ['Python']
 
 
 cool_reviewer.rate_hw(best_student, 'Python', 10)
-cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 9)
 cool_reviewer.rate_hw(best_student, 'Python', 10)
 
 cool_student.rate_lec(best_teacher, 'Python', 9)
 cool_student.rate_lec(best_teacher, 'Python', 8)
 cool_student.rate_lec(best_teacher, 'Python', 9)
 
+some_reviewer = cool_reviewer
+some_lecturer = best_teacher
+some_student = best_student
+
 print(best_student.grades)
 print(best_teacher.grades)
+print(some_reviewer)
+print(some_lecturer)
+print(some_student)
